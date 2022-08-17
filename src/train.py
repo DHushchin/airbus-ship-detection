f709781ac84c101dfa4f6bece56757bad5292c33
Dmytro Hushchin <dhuchchin@gmail.com>
2022-08-17 08:57:42 +0300

    updated model training

--- src/train.py
+++ src/train.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from keras.metrics import MeanIoU
 from keras.optimizers import Adam
+from keras.losses import binary_crossentropy
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 from utils import *
+
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 masks = pd.read_csv(ROOT_PATH + '/data/train_ship_segmentations_v2.csv')
@@ -18,34 +21,42 @@ train_ids, valid_ids = train_test_split(balanced_train_df,
                                         stratify = balanced_train_df['ships'])
 
 train_df = pd.merge(masks, train_ids)
-valid_df = pd.merge(masks, valid_ids)              
+valid_df = pd.merge(masks, valid_ids)       
 
-train_gen = make_image_gen(train_df)
-val_gen = make_image_gen(valid_df)
-
-train_x, train_y = next(train_gen)
-valid_x, valid_y = next(val_gen)
-
-test_gen = aug_gen(train_gen)
-test_x, test_y = next(test_gen)
-
-unet = build_model(train_x.shape[1:])
+unet = build_model()
 print(unet.summary())
 
-reduceLROnPlat = ReduceLROnPlateau(monitor='val_loss', factor=0.4, patience=5, 
-                                   mode='min', cooldown=3, min_lr=1e-8)
+reduceLROnPlat = ReduceLROnPlateau(monitor='val_loss', factor=0.4, 
+                                   patience=5, mode='min', cooldown=5)
 
-early = EarlyStopping(monitor="val_loss", mode="min", verbose=2, patience=50)
+early = EarlyStopping(monitor="val_loss", mode="min", verbose=2, patience=30)
 
 unet.compile(optimizer=Adam(), 
-             loss=dice_coef, 
-             metrics=[MeanIoU(num_classes=2)])                                                                
+             loss=binary_crossentropy, 
+             metrics=[MeanIoU(num_classes=2), dice_coef, 'binary_accuracy'])                                                                
 
-loss_history = unet.fit(aug_gen(make_image_gen(train_df)),
-                        steps_per_epoch=train_df.shape[0] // 1000,
-                        epochs=1000,
-                        validation_data=next(make_image_gen(valid_df)),
-                        callbacks=[early, reduceLROnPlat],)
+history = [unet.fit(image_gen(train_df, 8),
+                   steps_per_epoch=train_df.shape[0] // 1000,
+                   epochs=1000,
+                   validation_data=next(image_gen(valid_df, 8)),
+                   shuffle=True,
+                   callbacks=[early, reduceLROnPlat])]
 
 unet.save_weights(ROOT_PATH + '/model/weights.h5')
 unet.save(ROOT_PATH + '/model/model.h5')
+
+epochs = np.concatenate([mh.epoch for mh in history])
+fig, axes = plt.subplots(1, 2, figsize=(20, 8))
+
+axes[0].plot(epochs, np.concatenate([mh.history['loss'] for mh in history]), 'b-',
+             epochs, np.concatenate([mh.history['val_loss'] for mh in history]), 'r-')
+axes[0].legend(['Training', 'Validation'])
+axes[0].set_title('Loss')
+
+
+axes[1].plot(epochs, np.concatenate([mh.history['dice_coef'] for mh in history]), 'b-',
+             epochs, np.concatenate([mh.history['val_dice_coef'] for mh in history]), 'r-')
+axes[1].legend(['Training', 'Validation'])
+axes[1].set_title('Dice Coefficient')
+
+plt.show()
